517b97ae358eb802d86a5c7e958f22aeb31077b2
s-ginger <jum80frol@gmail.com>
2025-05-13 09:50:05 +0500

    new form

--- index/forms.py
+++ index/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from .models import Comment
+from .models import Comment, CustomUser
 
 User = get_user_model()
 
@@ -10,19 +10,20 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=150, required=True)
     password = forms.CharField(widget=forms.PasswordInput, required=True)
 
-#class CommentForm(forms.Form):
-#    user = models.ForeignKey(User, on_delete=models.CASCADE)
-#    comment = forms.CharField(max_length=150, required=True)
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
 
 class CustomUserForm(UserCreationForm):
+    bio = forms.CharField(widget=forms.Textarea, required=False, label="Биография")
+
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-  
+        model = CustomUser
+        fields = ('username', 'email', 'bio')  # Поля для создания пользователя
 
-
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()  # Сохраняем пользователя
+        return user
