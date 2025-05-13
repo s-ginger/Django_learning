5eb4b5d8929d90adc2efa341f0f1179d2cc390e6
s-ginger <jum80frol@gmail.com>
2025-05-13 09:13:19 +0500

    models 1

--- index/models.py
+++ index/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from mydjango.settings import AUTH_USER_MODEL
 
 # Create your models here.
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return f"{self.user} - {self.text} - {self.created_at}"
+
+
+class CustomUser(AbstractUser):
+    birth_date = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/%Y/%m/%d/', null=True, blank=True)
+    role = models.CharField(max_length=10, choices=[('teacher', 'Teacher'), ('student', 'Student')], default='student')
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+    def get_full_name(self):
+        """Возвращает полное имя пользователя."""
+        return f"{self.first_name} {self.last_name}"
+
