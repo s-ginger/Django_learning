7834e58cb4c6ed64a2ead69e03d9d6df4e99d945
s-ginger <jum80frol@gmail.com>
2025-05-15 10:43:34 +0500

    isRead les

--- index/models.py
+++ index/models.py
@@ -56,6 +56,7 @@ class Lesson(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to='lesson_images/', null=True, blank=True)
+    isRead = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
